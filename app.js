4f7d7ce9f23bd2dde0b05988ae104a8d4fa95ee0
konnono <konnono@gmail.com>
2021-06-13 14:28:43 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -31,7 +31,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/todos/new', (req, res) => {
-  return res.render('new')
+  res.render('new')
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  console.log('id:', id)
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
 })
 
 app.post('/todos', (req, res) => {
