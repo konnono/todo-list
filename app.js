d04bb42735d04814a591259eeff6ce132d3c844b
konnono <konnono@gmail.com>
2021-06-13 15:36:05 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -34,19 +34,46 @@ app.get('/todos/new', (req, res) => {
   res.render('new')
 })
 
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('./'))
+    .catch(error => console.log(error))
+})
+
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
-  console.log('id:', id)
   return Todo.findById(id)
     .lean()
     .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
-app.post('/todos', (req, res) => {
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
   const name = req.body.name
-  return Todo.create({ name })
-    .then(() => res.redirect('./'))
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
